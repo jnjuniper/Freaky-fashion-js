0631c9c1c525c97eab938efd51d6746cb53bb6ea
Jonathan <jonathan.berglund88@gmail.com>
2024-10-26 16:54:35 +0200

    Added products and remade home page with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+  spacing: {
+    '2': '0.5rem',
+    '4': '1rem',
+  },
+  fontSize: {
+    xs: '0.75rem',
+    lg: '1.125rem',
+  },
+  colors: {
+    gray: {
+      100: '#f2f2f2',
+      300: '#ccc',
+      700: '#333',
+    },
+  },
   plugins: [
     {
       tailwindcss: {},
