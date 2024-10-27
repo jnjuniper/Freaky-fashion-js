15443ffdfc23c4e306220c747ea861742e489f03
Jonathan <jonathan.berglund88@gmail.com>
2024-10-27 16:55:28 +0100

    Added new products, installed flowbite- for accordion.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.ejs"],
+  content: ["./views/*.ejs", "./node_modules/flowbite/**/*.js"], 
   theme: {
     extend: {},
+    plugins: [
+      require('flowbite/plugin')
+    ]
   },
   spacing: {
     '2': '0.5rem',
