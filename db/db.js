2410317ddcb47c14183ba8165b4b54cc5d4fcf14
Jonathan <168919546+jnjuniper@users.noreply.github.com>
2024-11-06 09:42:03 +0100

    Created database & added products

---
+++ db/db.js
@@ -0,0 +1,15 @@
+// ladda moduler vi behöver
+const sqlite3 = require('sqlite3').verbose();
+var path = require('path');
+
+// Ta fram absolut sökväg till databasfilen
+const dbFilePath = path.resolve(process.cwd(), './db/products.db');
+
+//Skapa db-objekt - vi använder detta för att kommunicera med databasen
+
+const db = new sqlite3.Database(dbFilePath, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+});
+
+// Exportera db-objektet - detta gör att vi kan importera det (require) i andra delar av applikationen
+module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Freaky Fashion' });
+
+    const sql = 'SELECT product_name, SKU, product_price';
+
+    db.all(sql, [], function (error, rows) {
+      res.render('Index', {
+      title: 'Freaky Fashion',
+      products: rows
+      });
+    }); 
 });
 
 module.exports = router;
